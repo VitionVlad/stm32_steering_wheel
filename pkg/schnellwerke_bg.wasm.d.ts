283f382a37541578ca88ed6d8f489bfc787cef97
vition vlad <48290199+VitionVlad@users.noreply.github.com>
2024-02-27 21:28:32 +0200

    Delete pkg directory

--- pkg/schnellwerke_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function main(): void;
-export function __wbindgen_malloc(a: number, b: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
-export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h028d6ab0163eb702(a: number, b: number): void;
-export function __wbindgen_exn_store(a: number): void;
